411ba1c1d4a947ec4ba3b25bee4954b3532266bc
xhusar2 <husarrichard@gmail.com>
2021-05-07 23:48:28 +0200

    fix generalization.py, add __init__ to models, comment out VPP parser initializations

---
+++ exquiro/models/class_diagram/generalization.py
@@ -0,0 +1,7 @@
+from .relation import Relation
+
+
+class Generalization(Relation):
+
+    def __init__(self, association_id, name, src_id, dest_id):
+        super().__init__(association_id, name, src_id, dest_id, "Generalization")
